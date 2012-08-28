f8c3a54783e8d81742768c336294fad3fc0cb797
minkooseo <minkoo.seo@gmail.com>
2012-08-29 00:36:02 +0900

    Adds examples to canonicalize function.

--- strings/canonicalze.R
+++ strings/canonicalze.R
@@ -1,4 +1,6 @@
 # Get canonicalized bag of words as vector given a word or vector of words.
+# e.g.) canonical_words("hello world")
+#       canonical_words(c("hello world", "good morning there"))
 canonical_words <- function(w) {
   return(unlist(lapply(strsplit(w, split=" |/"), tolower)))
 }
