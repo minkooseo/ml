9252f0ec9713d3821e2da65afb2b0f0ca75bfe90
Minkoo Seo <minkoo.seo@gmail.com>
2013-06-07 00:42:32 +0900

    Improves modeling performance and add all user prediction methods.

--- slopeone/slopeone.R
+++ slopeone/slopeone.R
@@ -1,4 +1,5 @@
 require(data.table)
+require(foreach)
 require(plyr)
 require(reshape2)
 
@@ -17,7 +18,7 @@ build_slopeone <- function(ratings, ...) {
   }
   # Generates all pairs of (item id 1, item id 2, diff) if both of item 1 and
   # 2 is rated by the same user.
-  score_diff_per_user <- ddply(ratings, .(user_id), function(rows) {
+  score_diff_per_user <- dlply(ratings, .(user_id), function(rows) {
     if (NROW(rows) > 1) {
       # A user may have rated an item multiple times. In such case, get average ratings 
       # for such items.
@@ -33,6 +34,11 @@ build_slopeone <- function(ratings, ...) {
               - rows[pair_rows_nums$rows_num1, 'rating'])
     }
   }, ...)
+  # ddply is slow when merging data frames within list, while rbindlist is
+  # faster.
+  score_diff_per_user <- as.data.frame(rbindlist(score_diff_per_user))
+  score_diff_per_user$item_id1 <- as.character(score_diff_per_user$item_id1)
+  score_diff_per_user$item_id2 <- as.character(score_diff_per_user$item_id2)
   # Compute average score diff between item 1 and item 2.
   model <- data.table(
       ddply(score_diff_per_user, 
@@ -44,38 +50,58 @@ build_slopeone <- function(ratings, ...) {
   return(model)
 }
 
-# Predict score for target_item_id given ratings of (item_id, rating).
+
+# Predict ratings for multiple users. 
+# This is useful for evaluting the performance of model.
+#
+# Param:
+#  model: A data table produced by build_slopeone.
+#  rating: A data frame containig (user_id, item_id, rating) whose rating
+#    needs to be predicted using the model.
+# Returns:
+#  A data table containig (user_id, item_id, rating, predicted_rating).
+predict_slopeone <-function(model, ratings) {
+  ddply(ratings,
+        .(user_id),
+        function(rows) {
+          if(NROW(rows) == 1) {
+            return(cbind(rows, data.frame(predicted_rating=NaN)))
+          }
+          predictions <- foreach(i=1:NROW(rows)) %do% {
+            # Predict for ith row using data excluding the row.
+            target_id <- rows[i, 'item_id']
+            ratings_for_user <- rows[-i, ]
+            data.frame(predicted_rating=c(predict_slopeone_for_user(
+              model, target_id, data.table(ratings_for_user))))
+          }
+          return(cbind(rows, rbindlist(predictions)))
+        })
+} 
+
+# Predict score for target_item_id given the ratings of a single user.
 #
 # Params:
-# model: A data table produced by build_slopeone. This contains
-#   (item id 1, item id 2, b).
-# target_item_id: Target item id to predict rating.
-# ratings: A data table containing (item id, rating) of the user.
+#   model: A data table produced by build_slopeone. This contains
+#     (item id 1, item id 2, b).
+#   target_item_id: Target item id to predict rating.
+#   ratings: A data table containing (item id, rating) of the user to predict
+#     target_item_id's rating.
 #
 # Returns:
-# Predicted rating score.
-predict_slopeone <- function(model, target_item_id, ratings) {
+#   Predicted rating score.
+predict_slopeone_for_user <- function(model, target_item_id, ratings) {
   # If target_id is already rated by the user, return that rating.
   already_rated <- subset(ratings, ratings$item_id == target_item_id)
   if (NROW(already_rated) == 1) {
     return(already_rated$rating)
   } else if (NROW(already_rated) > 1) {
-    stop(paste(target_item_id, " is already rated by user, but there are multiple ratings."))
+    warning(paste(target_item_id, 
+                  ' is already rated by user, but there are multiple ratings.'))
+    return(already_rated[1, ]$rating)
   }
-
   # Compute average ratings.
   ratings <- rename(ratings, c('item_id'= "item_id1"))
   ratings <- cbind(ratings, item_id2=target_item_id)
   setkey(ratings, item_id1, item_id2)
   return(mean(model[ratings, b+rating]$V1, na.rm=TRUE))
-}
-
-demo <- function() {
-  model <- build_slopeone(data.frame(
-    user_id=c('u1', 'u1', 'u1', 'u2', 'u2'),
-    item_id=c('i1', 'i2', 'i3', 'i1', 'i4'),
-    rating=c(3, 4, 5, 2, 4)))
-  
-  predict_slopeone(model,
-    'i2', data.table(data.frame(item_id=c('i1', 'i4'), rating=c(4, 4))))
 }
--- slopeone/tests/test_slopeone.R
+++ slopeone/tests/test_slopeone.R
@@ -16,7 +16,8 @@ test_that('duplicate item ratins', {
     item_id1=c('i1', 'i2'),
     item_id2=c('i2', 'i1'),
     b=c(.5, -.5),
-    support=c(1, 1))
+    support=c(1, 1),
+    stringsAsFactors=FALSE)
   expect_equal(as.data.frame(model), expected_model)
 })
 
@@ -29,36 +30,83 @@ test_that('multiple users', {
     item_id1=c('i1', 'i1', 'i2', 'i2', 'i3', 'i3'),
     item_id2=c('i2', 'i3', 'i1', 'i3', 'i1', 'i2'),
     b=c(1, 2, -1, 1, -2, -1),
-    support=c(2, 1, 2, 1, 1, 1))
+    support=c(2, 1, 2, 1, 1, 1),
+    stringsAsFactors=FALSE)
   expect_equal(as.data.frame(model), expected_model)
 })
 
-test_that('predict', {
-  model <- build_slopeone(
-    data.frame(user_id=c('u1', 'u1', 'u1', 'u2', 'u2'),
-               item_id=c('i1', 'i2', 'i3', 'i1', 'i2'),
-               rating=c(3, 4, 5, 4, 5)))
-  expected_model <- data.frame(
+test_that('predict_slopeone_for_user', {
+  model <- data.table(data.frame(
     item_id1=c('i1', 'i1', 'i2', 'i2', 'i3', 'i3'),
     item_id2=c('i2', 'i3', 'i1', 'i3', 'i1', 'i2'),
     b=c(1, 2, -1, 1, -2, -1),
-    support=c(2, 1, 2, 1, 1, 1))
+    support=c(2, 1, 2, 1, 1, 1),
+    stringsAsFactors=FALSE))
+  setkey(model, item_id1, item_id2)
   expect_equal(
-      predict_slopeone(model, 'i2',
-                       data.table(item_id=c('i1'), rating=c(4))),
+      predict_slopeone_for_user(model, 'i2',
+                                data.table(item_id=c('i1'), rating=c(4))),
       5)
   expect_equal(
-      predict_slopeone(model, 'i3',
-                       data.table(item_id=c('i1'), rating=c(4))),
+      predict_slopeone_for_user(model, 'i3',
+                                data.table(item_id=c('i1'), rating=c(4))),
       6)
   expect_equal(
-      predict_slopeone(model, 'i2',
-                       data.table(item_id=c('i1', 'i3'),
-                                  rating=c(4, 2))),
+      predict_slopeone_for_user(model, 'i2',
+                                data.table(item_id=c('i1', 'i3'),
+                                           rating=c(4, 2))),
       3)  # mean(5, 1)
   expect_equal(
-      predict_slopeone(model, 'i999',
-                       data.table(item_id=c('i1', 'i3'),
-                                  rating=c(4, 2))),
+      predict_slopeone_for_user(model, 'i999',
+                                data.table(item_id=c('i1', 'i3'),
+                                           rating=c(4, 2))),
       NaN)
+  expect_warning(
+    (first_rating <- predict_slopeone_for_user(model, 'i3',
+                              data.table(item_id=c('i3', 'i3'),
+                                         rating=c(4, 2)))),
+    c('i3  is already rated by user, but there are multiple ratings.'))
+  # First rating take precedence
+  expect_equal(first_rating, 4)
+})
+
+
+test_that('predict_slopeone', {
+  model <- data.table(data.frame(
+    item_id1=c('i1', 'i1', 'i2', 'i2', 'i3', 'i3'),
+    item_id2=c('i2', 'i3', 'i1', 'i3', 'i1', 'i2'),
+    b=c(1, 2, -1, 1, -2, -1),
+    support=c(2, 1, 2, 1, 1, 1),
+    stringsAsFactors=FALSE))
+  setkey(model, item_id1, item_id2)
+  ratings <- data.table(user_id=c('u1'), item_id=c('i1'), rating=c(4))
+  expected_ratings <- ratings
+  expected_ratings$predicted_rating <- c(NaN)
+  expect_that(
+    predict_slopeone(model, ratings),
+    is_equivalent_to(expected_ratings))
+  
+  ratings <- data.table(user_id=c('u1', 'u1'), item_id=c('i1', 'i2'),
+                        rating=c(3, 4))
+  expected_ratings <- ratings
+  expected_ratings$predicted_rating <- c(
+    3,  # 4 - 1
+    4)  # 3 + 1
+  expect_that(
+    predict_slopeone(model, ratings),
+    is_equivalent_to(expected_ratings))
+  
+  ratings <- data.table(user_id=c('u1', 'u2', 'u2', 'u2', 'u2'), 
+                        item_id=c('i1', 'i2', 'i3', 'i3', 'i999'),
+                        rating=c(  3,    4,    5,    1,   2))
+  expected_ratings <- ratings
+  expected_ratings$predicted_rating <- c(
+    NaN,  # Single rating.
+    2,  # ((5-1) + (1-1)) / 2
+    1,  # i3 is already rated.
+    5,  # i3 is already rated.
+    NaN)  # Previously unknown item.
+  expect_that(
+    predict_slopeone(model, ratings),
+    is_equivalent_to(expected_ratings))
 })
